dba0d3001f5334810a1b26fd864fbeba1652b74f
johnyguido <johnyguido@hotmail.com.br>
2020-09-26 23:30:33 -0300

    Adicionando a pagina de graficos

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -3,15 +3,15 @@ import axios from 'axios';
 import { RecordsResponse } from './types'
 import { formatDate } from './helpers';
 import Pagination from './Pagination';
-import {  Link } from 'react-router-dom'
 import './styles.css';
+import Filters from '../../components/Filters';
 
 const BASE_URL = 'http://localhost:8080'
 
 const Records = () => {
     const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
     const [activePage, setActivePage] = useState(0);
-    
+
     useEffect(() => {
         axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
             .then(response => setRecordsResponse(response.data));
@@ -23,13 +23,7 @@ const Records = () => {
 
     return (
         <div className="page-contaniner">
-            <div className ="filters-container records-actions">
-            <Link to="/charts">
-                <button className="action-filters">
-                    VER GRÁFICOS
-                </button>
-            </Link>
-            </div>
+            <Filters link="/charts" linkText="VER GRÁFICO"/>
             <table className="records-table" cellPadding="0" cellSpacing="0">
                 <thead>
                     <tr>
@@ -54,10 +48,10 @@ const Records = () => {
                     ))}
                 </tbody>
             </table>
-            <Pagination 
-            activePage={activePage}
-            goToPage={handlePageChange}
-            totalPages={recordsResponse?.totalPages}
+            <Pagination
+                activePage={activePage}
+                goToPage={handlePageChange}
+                totalPages={recordsResponse?.totalPages}
             />
         </div>
 
